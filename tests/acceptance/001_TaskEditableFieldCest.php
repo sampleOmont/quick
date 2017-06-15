53a6ee6b31aae6cd4fc8168aa975fffaea226c3d
Oleksii Montytskyi <omontytskyi@n-ix.com>
2017-06-15 14:36:00 +0300

    Add Travis CI configuration

--- tests/acceptance/001_TaskEditableFieldCest.php
+++ tests/acceptance/001_TaskEditableFieldCest.php
@@ -19,6 +19,7 @@ class TaskEditableFieldCest
     {
         $I->wantTo('Editable text field shown correct');
         $I->amOnPage('');
+        $I->waitForElementVisible(['xpath' => '//input[@value=\'Google Search\']'], 10);
         $I->waitForText('Task List', 10);
         $I->waitForElementVisible(['xpath'=>'//input[@type="text"]'], 10);
 
